2ff0971089cfb8afb8b08d795a8cc106525a3f1b
DucDoNguyen <ducdo252322@gmail.com>
2026-05-10 20:42:06 +0700

    Tao du an

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,10 +1,13 @@
 public class User {
-  //Post job cleanup.
-  ///usr/bin/tar --posix -cf cache.tzst --exclude cache.tzst -P -C /home/runner/work/Week-9/Week-9 --files-from manifest.txt --use-compress-program zstdmt
-  //Sent 16752207 of 16752207 (100.0%), 26.7 MBs/sec
-  //Cache saved with the key: setup-java-Linux-x64-maven-359510926e83fd44cce8c52adcdf88ec23831e94b83977c5e8db4b231bcc1fde
+  /* 22s
+  Post job cleanup.
+  /usr/bin/tar --posix -cf cache.tzst --exclude cache.tzst -P -C /home/runner/work/Week-9/Week-9 --files-from manifest.txt --use-compress-program zstdmt
+  Sent 16752207 of 16752207 (100.0%), 26.7 MBs/sec
+  Cache saved with the key: setup-java-Linux-x64-maven-359510926e83fd44cce8c52adcdf88ec23831e94b83977c5e8db4b231bcc1fde
+  */
 
   /*
+  14s
   /usr/bin/tar -xf /home/runner/work/_temp/141b543c-4766-48a3-b5a7-019b88d3016f/cache.tzst -P -C /home/runner/work/Week-9/Week-9 --use-compress-program unzstd
 Cache restored successfully
 Cache restored from key: setup-java-Linux-x64-maven-359510926e83fd44cce8c52adcdf88ec23831e94b83977c5e8db4b231bcc1fde
